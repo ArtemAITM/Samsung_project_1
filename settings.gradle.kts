148f9a3d365d855a37ffa5a974655c7ff60c3bf4
ArtemAITM <antropovam2008@gmail.com>
2023-12-12 20:37:42 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Samsung_project_1"
+include(":app")
+ 
